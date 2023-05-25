83069cb9ddffec64aa8d34b1b2ae4ff1f5b04610
irauldominguezb <20213tn103@utez.edu.mx>
2023-05-25 16:00:31 -0600

    creacion de save client

---
+++ src/modules/client/adapters/dtos/save-client.dto.ts
@@ -0,0 +1,5 @@
+export type SaveClientDto = {
+    fullname: string,
+    email: string,
+    visits: number
+}
--- src/modules/client/use-cases/ports/client.repository.ts
+++ src/modules/client/use-cases/ports/client.repository.ts
@@ -1,1 +1,8 @@
 import { Client } from "../../entities/client";
+import { SaveClientDto } from "../../adapters/dtos/save-client.dto";
+import { ResponseApi } from "../../../../kernel/types";
+
+export interface ClientRepository {
+    saveClient(payload: SaveClientDto): Promise<ResponseApi<Client>>
+    findAll(): Promise<ResponseApi<Client>>
+}
